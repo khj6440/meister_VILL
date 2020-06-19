b60f76d73fa06ceac7475670b1830ee963cfa2b7
khj6440 <gurwls7673@gmail.com>
2020-06-19 16:57:46 +0900

    Controller mapping 값 추가

--- meisterVill/src/main/java/kr/or/meister/member/controller/MemberController.java
+++ meisterVill/src/main/java/kr/or/meister/member/controller/MemberController.java
@@ -3,10 +3,12 @@ package kr.or.meister.member.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.or.meister.member.model.service.MemberService;
 
 @Controller
+@RequestMapping("/meister/member")
 public class MemberController {
 
 	@Autowired
