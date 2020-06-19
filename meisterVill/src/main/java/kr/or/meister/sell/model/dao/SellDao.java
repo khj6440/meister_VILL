1a34d4341f6d2d8b8918312ac32ea4a2dec7e484
KkimBang <60415103+KkimBang@users.noreply.github.com>
2020-06-19 17:08:24 +0900

    commit selldao 추가

--- meisterVill/src/main/java/kr/or/meister/sell/model/dao/SellDao.java
+++ meisterVill/src/main/java/kr/or/meister/sell/model/dao/SellDao.java
@@ -1,5 +1,7 @@
 package kr.or.meister.sell.model.dao;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -9,4 +11,8 @@ public class SellDao {
 
 	@Autowired
 	private SqlSessionTemplate sqlSession;
+
+	public List selectAllList() {
+		return null;
+	}
 }
