401c102d40bc3299b301d79ff475976483da42aa
kimyongjun <danji0123456@naver.com>
2020-06-23 16:49:38 +0900

    myCommit

--- meisterVill/src/main/java/kr/or/meister/board/controller/BoardController.java
+++ meisterVill/src/main/java/kr/or/meister/board/controller/BoardController.java
@@ -3,12 +3,10 @@ package kr.or.meister.board.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.or.meister.board.model.service.BoardService;
 
 @Controller
-@RequestMapping("/meister/board")
 public class BoardController {
 
 	@Autowired
--- meisterVill/src/main/java/kr/or/meister/member/controller/MemberController.java
+++ meisterVill/src/main/java/kr/or/meister/member/controller/MemberController.java
@@ -3,12 +3,10 @@ package kr.or.meister.member.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.or.meister.member.model.service.MemberService;
 
 @Controller
-@RequestMapping("/meister/member")
 public class MemberController {
 
 	@Autowired
