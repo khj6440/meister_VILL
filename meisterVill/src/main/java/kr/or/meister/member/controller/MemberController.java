5d2b3b9747e18e4820cdc24f3ba937bc52f437c7
khj6440 <gurwls7673@gmail.com>
2020-06-23 16:49:50 +0900

    메세지 구현전 DB연동 테스트

--- meisterVill/src/main/java/kr/or/meister/member/controller/MemberController.java
+++ meisterVill/src/main/java/kr/or/meister/member/controller/MemberController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.or.meister.member.model.service.MemberService;
+import kr.or.meister.member.model.vo.MemberVO;
 
 @Controller
 @RequestMapping("/meister/member")
@@ -14,4 +15,15 @@ public class MemberController {
 	@Autowired
 	@Qualifier("memberService")
 	private MemberService service;
+	
+	@RequestMapping("/login.do")
+	public void login(MemberVO m) {
+		MemberVO loginM =  service.selectOneMember(m);
+		
+		if(loginM!=null){
+			System.out.println("성공");
+		}else {
+			System.out.println("실패");
+		}
+	}
 }
--- meisterVill/src/main/java/kr/or/meister/member/model/dao/MemberDao.java
+++ meisterVill/src/main/java/kr/or/meister/member/model/dao/MemberDao.java
@@ -4,9 +4,17 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import kr.or.meister.member.model.vo.MemberVO;
+
 @Repository("memberDao")
 public class MemberDao {
 	
 	@Autowired
 	private SqlSessionTemplate sqlSession;
+
+	public MemberVO selectOneMember(MemberVO m) {
+		// TODO Auto-generated method stub
+		MemberVO lm= sqlSession.selectOne("member.selectOneMember",m);
+		return lm;
+	}
 }
