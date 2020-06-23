f2aaa43809f2570b365284627cb1c1d8cb6e1297
helloimsh2 <pspp1024@gmail.com>
2020-06-23 17:29:16 +0900

    profile.jsp css commit

--- meisterVill/src/main/java/kr/or/meister/member/controller/MemberController.java
+++ meisterVill/src/main/java/kr/or/meister/member/controller/MemberController.java
@@ -3,13 +3,25 @@ package kr.or.meister.member.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.or.meister.member.model.service.MemberService;
 
 @Controller
+@RequestMapping("/meister/member")
 public class MemberController {
 
 	@Autowired
 	@Qualifier("memberService")
 	private MemberService service;
+	
+	@RequestMapping("/mypage.do")
+	public String mypage() {
+		return "member/mypage";
+	}
+	
+	@RequestMapping("/myprofile.do")
+	public String myprofile() {
+		return "member/myprofile";
+	}
 }
